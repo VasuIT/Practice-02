a25695feb91d136a21c16c00e483e32d52844e94
VasuIT <vasuvardhan98@gmail.com>
2022-03-08 11:26:49 +0530

    practice commit

---
+++ src/main/java/com/democompany/Practice_02/App.java
@@ -0,0 +1,13 @@
+package com.democompany.Practice_02;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
